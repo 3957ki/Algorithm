95ad1357e13113ee250a914f163d2ba66904424a
3957ki <3957ki@gmail.com>
2024-08-11 16:27:22 +0900

    [Gold III] Title: 움직이는 미로 탈출, Time: 120 ms, Memory: 17916 KB -BaekjoonHub

--- 백준/Gold/16954. 움직이는 미로 탈출/움직이는 미로 탈출.java
+++ 백준/Gold/16954. 움직이는 미로 탈출/움직이는 미로 탈출.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayDeque;
 
 public class Main {
 
@@ -12,14 +11,16 @@ public class Main {
 		for(int i = 0; i < 8; i++) {
 			arr[i] = br.readLine().toCharArray();
 		}
-		Queue<Node> queue = new LinkedList<>();
+		ArrayDeque<Node> queue = new ArrayDeque<>();
 		int[] dx = {0, 0, 1, -1, 1, 1, -1, -1, 0};
 		int[] dy = {1, -1, 0, 0, 1, -1, 1, -1, 0};
 		queue.add(new Node(7, 0, 0));
 		int time = 0;
+//		BFS
 		while(!queue.isEmpty()) {
-			Node node = queue.poll();
+			Node node = queue.pollFirst();
 			int depth = node.depth;
+//			시간이 한차례 지난 노드라면 벽 한칸씩 내려오기
 			if(time != depth) {
 				time = depth;
 				for(int i = 6; i >= 0; i--) {
@@ -29,16 +30,19 @@ public class Main {
 					}
 				}
 			}
+//			9방 탐색
 			for(int d = 0; d < 9; d++) {
 				int x = node.x + dx[d];
 				int y = node.y + dy[d];
 				if(x < 0 || x > 7 || y < 0 || y > 7) continue;
+//				최상위층에 도달하거나, 8초가 지난다면 true;
 				if(y == 0 || depth == 8) {
 					System.out.println(1);
 					return;
 				}
+//				탐색한 노드가 벽이 아니고, 노드 바로 위가 벽이 아닐 때 1초 증가 후 add
 				if(arr[y][x] == '.' && arr[y-1][x] == '.') {
-					queue.add(new Node(y, x, depth+1));
+					queue.addLast(new Node(y, x, depth+1));
 				}
 			}
 		}
