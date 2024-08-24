1a8da05a4d45c0b08677c692643b3a8aca63a754
3957ki <3957ki@gmail.com>
2024-08-25 02:15:58 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 460 ms, Memory: 62220 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		int[] parent = new int[N+1];
+		List<Integer>[] arr = new ArrayList[N+1];
+		for(int i = 0; i <= N; i++) {
+			arr[i] = new ArrayList<>();
+		}
+		boolean[] visited = new boolean[N+1];
+		
+		for(int i = 1; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			
+			arr[start].add(end);
+			arr[end].add(start);
+		}
+		
+		ArrayDeque<Integer> queue = new ArrayDeque<>();
+		queue.addLast(1);
+		while(!queue.isEmpty()) {
+			Integer prt = queue.removeFirst();
+			for(Integer num : arr[prt]) {
+				if(visited[num]) continue;
+				visited[num] = true;
+				parent[num] = prt;
+				queue.addLast(num);
+			}
+		}
+		
+		for(int i = 2; i <= N; i++) {
+			sb.append(parent[i]).append('\n');
+		}
+		System.out.println(sb);
+	}
+	
+}
