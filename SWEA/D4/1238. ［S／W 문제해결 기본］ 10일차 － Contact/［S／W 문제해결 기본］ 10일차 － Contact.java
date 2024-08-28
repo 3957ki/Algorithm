b871d4de1ba949a20266569cac38bf91907c36e4
3957ki <3957ki@gmail.com>
2024-08-28 15:17:55 +0900

    [D4] Title: [S/W 문제해결 기본] 10일차 - Contact, Time: 127 ms, Memory: 20,304 KB -BaekjoonHub

---
+++ SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = 10;
+		for (int t = 1; t <= T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken())/2;
+			int M = Integer.parseInt(st.nextToken());
+			
+			boolean[][] edge = new boolean[101][101];
+			st = new StringTokenizer(br.readLine());
+			for(int i = 0; i < N; i++) {
+				int start = Integer.parseInt(st.nextToken());
+				int end = Integer.parseInt(st.nextToken());
+				edge[start][end] = true;
+			}
+			
+			Queue<Integer> queue = new ArrayDeque<>();
+			boolean[] visited = new boolean[101];
+			queue.add(M);
+			visited[M] = true;
+			
+			int answer = 0;
+			while(!queue.isEmpty()) {
+				answer = 0;
+				int L = queue.size();
+				for(int i = 0; i < L; i++) {
+					int num = queue.poll();
+					answer = Math.max(answer, num);
+					for(int j = 1; j <= 100; j++) {
+						if(edge[num][j]) {
+							if(visited[j]) continue;
+							visited[j] = true;
+							queue.add(j);
+						}
+					}
+				}
+			}
+			
+			sb.append('#').append(t).append(' ').append(answer).append('\n');
+		}
+		System.out.println(sb);
+	}
+
+}
