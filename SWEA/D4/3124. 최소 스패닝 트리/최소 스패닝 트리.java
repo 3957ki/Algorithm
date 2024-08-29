bef50ee3242c938b3bad09cf50330aff8ed80ad4
3957ki <3957ki@gmail.com>
2024-08-29 14:11:35 +0900

    [D4] Title: 최소 스패닝 트리, Time: 2,074 ms, Memory: 125,576 KB -BaekjoonHub

---
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	static int N;
+	static int[] parents;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for(int t = 1; t <= T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			
+			Edge[] edges = new Edge[M];
+			for(int i = 0; i < M; i++) {
+				st = new StringTokenizer(br.readLine());
+				edges[i] = new Edge(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+			}
+			
+			Arrays.sort(edges, (o1, o2) -> o1.weight - o2.weight);
+			parents = new int[N+1];
+			make();
+			
+			int cnt = 0;
+			long cost = 0;
+			for(Edge edge : edges) {
+				if(union(edge.start, edge.end)) {
+					cost += edge.weight;
+					if(++cnt == N-1) break;
+				}
+				
+			}
+			sb.append('#').append(t).append(' ').append(cost).append('\n');
+		}
+		System.out.println(sb);
+	}
+	
+	static void make() {
+		for(int i = 1; i <= N; i++) {
+			parents[i] = -1;
+		}
+	}
+	
+	static int findSet(int a) {
+		if(parents[a] < 0) return a;
+		return parents[a] = findSet(parents[a]);
+	}
+	
+	static boolean union(int a, int b) {
+		int aRoot = findSet(a);
+		int bRoot = findSet(b);
+		
+		if(aRoot == bRoot) return false;
+//		집합의 크기 관리
+		parents[aRoot] += parents[bRoot];
+		parents[bRoot] = aRoot;
+		return true;
+	}
+	
+	static class Edge{
+		int start, end, weight;
+
+		public Edge(int start, int end, int weight) {
+			this.start = start;
+			this.end = end;
+			this.weight = weight;
+		}
+		
+	}
+	
+}
