32d48321cba305fa9b947db27e4b9537892c10ef
3957ki <3957ki@gmail.com>
2024-09-04 14:38:14 +0900

    [Silver III] Title: 1로 만들기, Time: 92 ms, Memory: 15880 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,21 +1,30 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		
-		System.out.println(recur(n, 0));
-	}
 
-	static int recur(int n, int cnt) {
-		if(n < 2) {
-			return cnt;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		if(N == 1) {
+			System.out.println(0);
+			return;
+		}
+		if(N == 2) {
+			System.out.println(1);
+			return;
 		}
 		
-		return Math.min(recur(n/2, cnt + 1 + n%2), recur(n/3, cnt + 1 + n%3));
+		int[] dp = new int[N+1];
+		dp[1] = 0;
+		dp[2] = 1;
+		dp[3] = 1;
+		
+		for(int i = 4; i <= N; i++) {
+			dp[i] = Math.min(dp[i/2]+i%2+1, dp[i/3]+i%3+1);
+		}
+		
+		System.out.println(dp[N]);
 	}
+
 }
