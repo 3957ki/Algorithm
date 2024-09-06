5e778aed16ef29713e5f205dc5205da0559ff288
3957ki <3957ki@gmail.com>
2024-09-06 23:31:40 +0900

    [Bronze V] Title: 새싹, Time: 64 ms, Memory: 11432 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("         ,r'\"7\n"
+				+ "r`-_   ,'  ,/\n"
+				+ " \\. \". L_r'\n"
+				+ "   `~\\/\n"
+				+ "      |\n"
+				+ "      |");
+	}
+
+}
