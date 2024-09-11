b320be09f86fbccc1be184a241d9d061815ee6bd
3957ki <3957ki@gmail.com>
2024-09-11 23:00:06 +0900

    [Gold III] Title: 골목 대장 호석 - 효율성 1, Time: 2980 ms, Memory: 301144 KB, Score: 16 point -BaekjoonHub

--- 백준/Gold/20182. 골목 대장 호석 － 효율성 1/골목 대장 호석 － 효율성 1.java
+++ 백준/Gold/20182. 골목 대장 호석 － 효율성 1/골목 대장 호석 － 효율성 1.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 public class Main {
 	
@@ -25,60 +26,66 @@ public class Main {
 			edges[i] = new ArrayList<>();
 		}
 		
+//		간선이 큰 순으로 저장
+		TreeSet<Integer> set = new TreeSet<>((o1, o2) -> o2 - o1);
+		
 		for(int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int A = Integer.parseInt(st.nextToken());
 			int B = Integer.parseInt(st.nextToken());
 			int weight = Integer.parseInt(st.nextToken());
 			
-			edges[A].add(new Edge(B, weight, 0));
-			edges[B].add(new Edge(A, weight, 0));
-			
+			edges[A].add(new Edge(B, weight));
+			edges[B].add(new Edge(A, weight));
+			set.add(weight);
 		}
 		
-//		다익스트라
+		int answer = -1;
+		
 		PriorityQueue<Edge> pq = new PriorityQueue<>((o1, o2) -> o1.w - o2.w);
 		boolean[] visited = new boolean[N+1];
 		int[] dist = new int[N+1];
-		Arrays.fill(dist, INF);
-		dist[start] = 0;
-		pq.add(new Edge(start, 0, 0));
-		
-		int answer = Integer.MAX_VALUE;
-		
-		while(!pq.isEmpty()) {
-			Edge e = pq.poll();
-			int now = e.v;
+		for(Integer maxCost : set) {
+//			다익스트라
+			pq.clear();
+			Arrays.fill(visited, false);
+			Arrays.fill(dist, INF);
+			dist[start] = 0;
+			pq.add(new Edge(start, 0));
 			
-			if(visited[now]) continue;
-			visited[now] = true;
-			
-			for(Edge edge : edges[now]) {
-				int next = edge.v;
-				if(dist[next] > dist[now] + edge.w) {
-//					가진 돈보다 많이들면 패스
-					if(dist[now]+edge.w > C) continue;
-					dist[next] = dist[now] + edge.w;
-//					종료지점이라면 answer 갱신
-					if(next == end) {
-						answer = Math.min(answer, Math.max(e.c, edge.w));
+			while(!pq.isEmpty()) {
+				int now = pq.poll().v;
+//				도착지라면 종료
+				if(now == end) break;
+				if(visited[now]) continue;
+				visited[now] = true;
+				
+				for(Edge edge : edges[now]) {
+					int next = edge.v;
+					if(dist[next] > dist[now] + edge.w) {
+//						maxCost보다 큰 간선은 패스
+						if(edge.w > maxCost) continue;
+						dist[next] = dist[now] + edge.w;
+						pq.add(new Edge(next, dist[next]));
 					}
-					pq.add(new Edge(next, dist[next], Math.max(e.c, edge.w)));
 				}
 			}
+//			가진 돈보다 많이 필요하면 종료
+			if(dist[end] > C) break;
+//			answer 갱신
+			answer = maxCost;
 		}
 		
-		System.out.println(answer == Integer.MAX_VALUE ? -1 : answer);
+		System.out.println(answer);
 		
 	}
 	
 	static class Edge{
-		int v, w, c;
+		int v, w;
 
-		public Edge(int v, int w, int c) {
+		public Edge(int v, int w) {
 			this.v = v;
 			this.w = w;
-			this.c = c;
 		}
 		
 	}
