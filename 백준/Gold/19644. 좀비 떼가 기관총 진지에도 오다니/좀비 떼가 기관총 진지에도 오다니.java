c891c6eefc90c8e5c4a23de2e5edf2da60a73723
3957ki <3957ki@gmail.com>
2024-11-15 11:03:41 +0900

    [Gold III] Title: 좀비 떼가 기관총 진지에도 오다니, Time: 604 ms, Memory: 269560 KB -BaekjoonHub

--- 백준/Gold/19644. 좀비 떼가 기관총 진지에도 오다니/좀비 떼가 기관총 진지에도 오다니.java
+++ 백준/Gold/19644. 좀비 떼가 기관총 진지에도 오다니/좀비 떼가 기관총 진지에도 오다니.java
@@ -1,5 +1,9 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -11,35 +15,31 @@ public class Main {
         int Mk = Integer.parseInt(st.nextToken());
         int C = Integer.parseInt(br.readLine());
         
-        // ArrayDeque 사용 (LinkedList보다 빠름)
-        Deque<Integer> noGunZone = new ArrayDeque<>();
+//      수류탄 사용 위치
+        Queue<Integer> noGunZone = new ArrayDeque<>();
         
-        boolean possible = true;
-        int zombieHp;
-        
-        for (int i = 0; i < L && possible; i++) {
-            zombieHp = Integer.parseInt(br.readLine());
+        for (int i = 0; i < L; i++) {
+            int zombieHp = Integer.parseInt(br.readLine());
             
             // 기관총 사거리를 벗어난 지점 제거
-            while (!noGunZone.isEmpty() && noGunZone.peekFirst() <= i - Ml) {
-                noGunZone.pollFirst();
+            while (!noGunZone.isEmpty() && noGunZone.peek() <= i - Ml) {
+                noGunZone.poll();
             }
             
             // 현재 위치에서의 실제 데미지 계산
-            // noGunZone.size()는 기관총을 쏘지 않은 구간의 개수
             int damage = Mk * (Math.min(i + 1, Ml) - noGunZone.size());
             
             if (zombieHp > damage) {
-                if (C > 0) {
-                    C--;
-                    noGunZone.offerLast(i);  // 수류탄 사용 위치 기록
-                } else {
-                    possible = false;
+                if (C-- > 0) {
+                    noGunZone.add(i);  // 수류탄 사용 위치 기록
+                }
+                else {
+                    System.out.println("NO");
+                    return;
                 }
             }
         }
         
-        System.out.println(possible ? "YES" : "NO");
-        br.close();
+        System.out.println("YES");
     }
 }
