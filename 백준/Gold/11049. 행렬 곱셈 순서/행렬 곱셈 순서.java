1d4020824b1ee6127741d120380e32bf5c5f4750
3957ki <3957ki@gmail.com>
2024-11-17 03:08:35 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 212 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
@@ -22,16 +22,22 @@ public class Main {
 			arr[i] = new Node(first, second);
 		}
 		
+//		dp[묶음수][시작점]
 		int[][] dp = new int[N+1][N+1];
 		for(int i = 1; i < N; i++) {
 			dp[2][i] = arr[i].first*arr[i].second*arr[i+1].second;
 		}
 		
+//		묶음 수
 		for(int i = 3; i <= N; i++) {
+//			시작점
 			for(int j = 1; j <= N-i+1; j++) {
 				int min = Integer.MAX_VALUE;
+//				묶음을 k 만큼 잘라서 계산해보기
 				for(int k = 1; k < i; k++) {
+//					시작점의 첫번째 수 x 끝점의 두번째 수 x k만큼 떨어진 값의 첫번째 수
 					int temp = arr[j].first*arr[j+i-1].second*arr[j+k].first;
+//					temp + 자른 첫번째 dp값 + 자른 두번째 dp값
 					min = Math.min(temp+dp[k][j]+dp[i-k][j+k], min);
 				}
 				dp[i][j] = min;
