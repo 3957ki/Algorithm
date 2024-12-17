e47a4c810850d994d0e93fa5843b7d510e6d553b
3957ki <3957ki@gmail.com>
2024-12-17 22:49:36 +0900

    [Gold IV] Title: 방 번호, Time: 212 ms, Memory: 46624 KB -BaekjoonHub

--- 백준/Gold/15668. 방 번호/방 번호.java
+++ 백준/Gold/15668. 방 번호/방 번호.java
@@ -4,12 +4,10 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Main {
-    static String[] arr = new String[] {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
-    static int N;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
         K: for(int a = 1; a < 100000; a++) {
             int b = N - a;
