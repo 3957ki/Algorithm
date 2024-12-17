0b02872588fb5a369260e181f18a6228e409a7d6
3957ki <3957ki@gmail.com>
2024-12-17 22:48:19 +0900

    [Gold IV] Title: 방 번호, Time: 228 ms, Memory: 45600 KB -BaekjoonHub

---
+++ 백준/Gold/15668. 방 번호/방 번호.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    static String[] arr = new String[] {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    static int N;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        K: for(int a = 1; a < 100000; a++) {
+            int b = N - a;
+            if(b <= 0) continue;
+            String A = String.valueOf(a);
+            String B = String.valueOf(b);
+            if(A.length() + B.length() > 10) continue;
+            Set<Character> set = new HashSet<>();
+            for(int i = 0; i < A.length(); i++){
+                if(set.contains(A.charAt(i))) continue K;
+                set.add(A.charAt(i));
+            }
+            for(int i = 0; i < B.length(); i++){
+                if(set.contains(B.charAt(i))) continue K;
+                set.add(B.charAt(i));
+            }
+            System.out.println(A+" + " + B);
+            System.exit(0);
+        }
+        System.out.println(-1);
+    }
+
+}
