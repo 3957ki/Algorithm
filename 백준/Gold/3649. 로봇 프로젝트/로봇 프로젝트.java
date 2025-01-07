36c2a56d9fa3f1152c922ada83d9594f0d376226
3957ki <3957ki@gmail.com>
2025-01-08 00:04:24 +0900

    [Gold V] Title: 로봇 프로젝트, Time: 3076 ms, Memory: 409720 KB -BaekjoonHub

---
+++ 백준/Gold/3649. 로봇 프로젝트/로봇 프로젝트.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input;
+        while((input = br.readLine()) != null){
+            int X = Integer.parseInt(input)*10000000;
+            int N = Integer.parseInt(br.readLine());
+
+            Map<Integer, Integer> map = new HashMap<>();
+            int[] arr = new int[N];
+
+            for (int i = 0; i < N; i++) {
+                int block = Integer.parseInt(br.readLine());
+                arr[i] = block;
+                map.put(block, map.getOrDefault(block, 0) + 1);
+            }
+
+            int[] answer = new int[2];
+            int maxDiff = -1;
+
+            for (int block : arr) {
+                if(map.containsKey(X-block)) {
+                    if(X - block == block && map.get(block) < 2) continue;
+                    int diff = Math.abs(X-2*block);
+                    if(diff > maxDiff) {
+                        maxDiff = diff;
+                        answer[0] = Math.min(X-block, block);
+                        answer[1] = Math.max(X-block, block);
+                    }
+                }
+            }
+
+            if(maxDiff == -1) System.out.println("danger");
+            else System.out.println("yes " + answer[0] + " " + answer[1]);
+        }
+
+    }
+}
