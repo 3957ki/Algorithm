36436f5699a376579242e535d8cbc088a973db62
3957ki <3957ki@gmail.com>
2025-01-17 00:28:19 +0900

    [Gold V] Title: 나무 위의 빗물, Time: 424 ms, Memory: 130952 KB -BaekjoonHub

--- 백준/Gold/17073. 나무 위의 빗물/나무 위의 빗물.java
+++ 백준/Gold/17073. 나무 위의 빗물/나무 위의 빗물.java
@@ -6,10 +6,6 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static List<Integer>[] edges;
-    static double total = 0;
-    static int C = 0;
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -18,49 +14,23 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         double W = Double.parseDouble(st.nextToken());
 
-        edges = new List[N+1];
-        for (int i = 1; i <= N; i++) {
-            edges[i] = new ArrayList<>();
-        }
+        int[] counts = new int[N+1];
 
         for (int i = 1; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
 
-            edges[start].add(end);
-            edges[end].add(start);
+            counts[start]++;
+            counts[end]++;
         }
 
-        DFS(1, 0, W);
+        int leaf = 0;
+        for (int i = 2; i <= N; i++) {
+            if(counts[i] == 1) leaf++;
+        }
 
-        System.out.println(total/C);
+        System.out.println(W/leaf);
     }
 
-    static void DFS(int now, int prev, double sum) {
-
-        // 받은게 없으면 return
-        if(sum == 0) return;
-
-        // 자식 수
-        int cnt = edges[now].size();
-
-        
-        if(now != 1){
-            // 1이면 자식이 없음
-            if(cnt == 1) {
-                total += sum;
-                C++;
-                return;
-            }
-            cnt--;
-        }
-
-        double w = sum/cnt;
-
-        for (Integer next : edges[now]) {
-            if (next == prev) continue;
-            DFS(next, now, w);
-        }
-    }
 }
