2787d3ae15c30d9822ecffd418c62854321288c6
3957ki <3957ki@gmail.com>
2025-03-15 04:18:34 +0900

    [Platinum IV] Title: 수열과 쿼리 21, Time: 516 ms, Memory: 65476 KB -BaekjoonHub

--- 백준/Platinum/16975. 수열과 쿼리 21/수열과 쿼리 21.java
+++ 백준/Platinum/16975. 수열과 쿼리 21/수열과 쿼리 21.java
@@ -4,89 +4,61 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static long[] seg, lazy, arr;
 	static int N;
+	static long[] tree;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
 		N = Integer.parseInt(br.readLine());
-		
-		arr = new long[N+1];
-		seg = new long[N*4];
-		lazy = new long[N*4];
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i = 1; i <= N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+
+		tree = new long[N + 1];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		// 구간 업데이트
+		for (int i = 1; i <= N; i++) {
+			rangeUpdate(i, i, Integer.parseInt(st.nextToken()));
 		}
-		
-		init(1, N, 1);
-		
+
 		int M = Integer.parseInt(br.readLine());
-		for(int i = 0; i < M; i++) {
+
+		while (M-- > 0) {
 			st = new StringTokenizer(br.readLine());
-			int cmd = Integer.parseInt(st.nextToken());
-			
-//			update
-			if(cmd == 1) {
-				int start = Integer.parseInt(st.nextToken());
-				int end = Integer.parseInt(st.nextToken());
-				int diff = Integer.parseInt(st.nextToken());
-				update(1, N, 1, start, end, diff);
-			}
-//			find
-			else {
-				int num = Integer.parseInt(st.nextToken());
-				find(1, N, 1, num);
-				sb.append(arr[num]).append('\n');
+			switch (Integer.parseInt(st.nextToken())) {
+				case 1:
+					rangeUpdate(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()),
+						Integer.parseInt(st.nextToken()));
+					break;
+				case 2:
+					sb.append(query(Integer.parseInt(st.nextToken()))).append('\n');
+					break;
 			}
 		}
+
 		System.out.println(sb);
 	}
 
-	static void propagate(int start, int end, int index) {
-//		자식이 있다면
-		if(start != end) {
-			lazy[index<<1] += lazy[index];
-			lazy[index<<1|1] += lazy[index];
+	static void update(int num, long diff) {
+		while (num <= N) {
+			tree[num] += diff;
+			num += num & -num;
 		}
-		seg[index] += lazy[index];
-		lazy[index] = 0;
 	}
-	
-	static void init(int start, int end, int index) {
-		if(start == end) {
-			seg[index] = arr[start];
-			return;
-		}
-		int mid = (start+end)/2;
-		init(start, mid, index<<1);
-		init(mid+1, end, index<<1|1);
+
+	static void rangeUpdate(int start, int end, long diff) {
+		update(start, diff);
+		update(end + 1, -diff);
 	}
-	
-	static void update(int start, int end, int index, int left, int right, int diff) {
-		propagate(start, end, index);
-		if(start > right || end < left) return;
-		if(start >= left && end <= right) {
-			lazy[index] += diff;
-			propagate(start, end, index);
-			return;
+
+	// point 조회
+	static long query(int num) {
+		long sum = 0;
+		while (num > 0) {
+			sum += tree[num];
+			num -= num & -num;
 		}
-		int mid = (start+end)/2;
-		update(start, mid, index<<1, left, right, diff);
-		update(mid+1, end, index<<1|1, left, right, diff);
-	}
-	
-	static void find(int start, int end, int index, int num) {
-		propagate(start, end, index);
-		if(start > num || end < num) return;
-		if(start == num && end == num) {
-			arr[num] = seg[index];
-			return;
-		}
-		int mid = (start+end)/2;
-		find(start, mid, index<<1, num);
-		find(mid+1, end, index<<1|1, num);
+
+		return sum;
 	}
 }
