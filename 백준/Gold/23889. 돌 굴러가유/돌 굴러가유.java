3b347eb41c01d29e9ef304fb77d5945133daa7bc
3957ki <3957ki@gmail.com>
2025-03-26 00:38:48 +0900

    [Gold IV] Title: 돌 굴러가유, Time: 648 ms, Memory: 46416 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/23889. 돌 굴러가유/돌 굴러가유.java
+++ 백준/Gold/23889. 돌 굴러가유/돌 굴러가유.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -15,11 +14,18 @@ public class Main {
 		int M = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 
-		int[] dp = new int[N + 1];
+		int[] arr = new int[N + 1];
 
 		st = new StringTokenizer(br.readLine());
 		for (int i = 1; i <= N; i++) {
-			dp[i] = dp[i - 1] + Integer.parseInt(st.nextToken());
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		int[] dp = new int[N + 1];
+		dp[N] = arr[N];
+
+		for (int i = N - 1; i >= 1; i--) {
+			dp[i] = arr[i] + dp[i + 1];
 		}
 
 		int[] stones = new int[K];
@@ -30,29 +36,35 @@ public class Main {
 
 		List<Node> list = new ArrayList<>();
 
-		int prev = N;
+		int prev = 0;
 		for (int i = K - 1; i >= 0; i--) {
-			list.add(new Node(stones[i], dp[prev] - dp[stones[i] - 1]));
-			prev = stones[i] - 1;
+			list.add(new Node(stones[i], dp[stones[i]] - prev));
+			prev = dp[stones[i]];
 		}
 
-		Collections.sort(list, (o1, o2) -> o2.value - o1.value);
+		list.sort((o1, o2) -> {
+			if (o1.value == o2.value)
+				return o1.index - o2.index;
 
-		int[] arr = new int[M];
+			return o2.value - o1.value;
+		});
+
+		int[] answer = new int[M];
 		for (int i = 0; i < M; i++) {
-			arr[i] = list.get(i).index;
+			answer[i] = list.get(i).index;
 		}
 
-		Arrays.sort(arr);
+		Arrays.sort(answer);
 
-		for (int now : arr) {
+		for (int now : answer) {
 			sb.append(now).append('\n');
 		}
 		System.out.println(sb);
 	}
 
 	static class Node {
-		int index, value;
+		int index;
+		int value;
 
 		public Node(int index, int value) {
 			this.index = index;
