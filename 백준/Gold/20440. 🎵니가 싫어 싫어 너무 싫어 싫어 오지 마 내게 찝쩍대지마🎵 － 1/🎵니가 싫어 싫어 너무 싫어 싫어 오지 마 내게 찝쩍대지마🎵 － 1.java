153006bdafe0cb2512ca30c4fe05b3b852610e1e
3957ki <3957ki@gmail.com>
2025-04-17 22:32:29 +0900

    [Gold III] Title: 🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 - 1, Time: 1160 ms, Memory: 385144 KB -BaekjoonHub

--- 백준/Gold/20440. 🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 － 1/🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 － 1.java
+++ 백준/Gold/20440. 🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 － 1/🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 － 1.java
@@ -1,52 +1,57 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
-import java.util.TreeMap;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 
-		Map<Integer, Integer> map = new TreeMap<>((o1, o2) -> o1 - o2);
+		List<Integer> list = new ArrayList<>();
+
+		Map<Integer, Integer> map = new HashMap<>();
 
 		int N = Integer.parseInt(br.readLine());
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int start = Integer.parseInt(st.nextToken());
 			int end = Integer.parseInt(st.nextToken());
+			if (!map.containsKey(start))
+				list.add(start);
+			if (!map.containsKey(end))
+				list.add(end);
 			map.put(start, map.getOrDefault(start, 0) + 1);
 			map.put(end, map.getOrDefault(end, 0) - 1);
 		}
+
+		Collections.sort(list);
 
 		int max = 0;
 		int cur = 0;
 		int start = 0;
 		int end = 0;
 		boolean flag = false;
-		for (Map.Entry<Integer, Integer> now : map.entrySet()) {
-			cur += now.getValue();
+
+		for (Integer now : list) {
+			cur += map.get(now);
 			if (cur > max) {
 				max = cur;
-				start = now.getKey();
+				start = now;
 				flag = true;
 			} else if (flag && cur < max) {
-				end = now.getKey();
+				end = now;
 				flag = false;
 			}
+
 		}
 
 		sb.append(max).append('\n').append(start).append(' ').append(end);
 		System.out.println(sb);
 	}
 
-	static class Node {
-		int start, end;
-
-		public Node(int start, int end) {
-			this.start = start;
-			this.end = end;
-		}
-	}
 }
