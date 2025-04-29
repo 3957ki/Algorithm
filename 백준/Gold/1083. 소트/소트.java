a50a6d29ccfd876b91adc68094173784eec8702e
3957ki <3957ki@gmail.com>
2025-04-29 22:33:21 +0900

    [Gold IV] Title: 소트, Time: 68 ms, Memory: 11660 KB -BaekjoonHub

--- 백준/Gold/1083. 소트/소트.java
+++ 백준/Gold/1083. 소트/소트.java
@@ -1,6 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -12,7 +12,7 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		List<Integer> list = new ArrayList<>();
+		List<Integer> list = new LinkedList<>();
 		for (int i = 0; i < N; i++) {
 			list.add(Integer.parseInt(st.nextToken()));
 		}
@@ -21,9 +21,13 @@ public class Main {
 
 		int L = N;
 		for (int i = 0; i < N; i++) {
+
+			// 현재 최댓값과 인덱스
 			int max = list.get(0);
 			int idx = 0;
+
 			for (int j = 1; j < L; j++) {
+				// 교환 횟수가 남는 경우만
 				if (j <= S) {
 					int now = list.get(j);
 					if (now > max) {
