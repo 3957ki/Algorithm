f0f2bd3cf96d115e7d0f476a4a35564936c94c7b
3957ki <3957ki@gmail.com>
2025-08-26 23:36:33 +0900

    [Gold III] Title: 고득점, Time: 68 ms, Memory: 11668 KB -BaekjoonHub

--- 백준/Gold/3663. 고득점/고득점.java
+++ 백준/Gold/3663. 고득점/고득점.java
@@ -10,27 +10,22 @@ public class Main {
 		while(T-- > 0) {
 			String str = br.readLine();
 			int N = str.length();
-			int[] arr = new int[N];
 
-			int cnt = 0;
-			int min = N-1;
+			int cnt = 0;	// 상하 이동 횟수
+			int min = N-1;	// 좌우 이동 횟수
 			int prev = 0;
 			int last = 0;
-			boolean flag = false;
 			for(int i = 0; i < N; i++) {
-				arr[i] = str.charAt(i) - 'A';
-				if(arr[i] == 0) continue;
+				int now = str.charAt(i) - 'A';
+				if(now == 0) continue;
 				last = i;
-				flag = true;
-				arr[i] = Math.min(arr[i], 26 - arr[i]);
-				cnt += arr[i];
-
+				cnt += Math.min(now, 26 - now);
 				min = Math.min(min, prev * 2 + N - i);
-				min = Math.min(min, prev + (N - i)*2);
+				min = Math.min(min, prev + (N - i) * 2);
 				prev = i;
 			}
 			min = Math.min(min, last);
-			sb.append(flag ? cnt + min : cnt).append('\n');
+			sb.append(cnt == 0 ? cnt : cnt + min).append('\n');
 		}
 		System.out.println(sb);
     }
